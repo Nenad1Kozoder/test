73577739e218c1857cb17f6c6bb2045887c04843
Nenad Kozoder <44727600+Nenad1Kozoder@users.noreply.github.com>
2020-05-12 21:36:03 +0200

    new

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,28 @@
-$(document).ready(function() {
-    
+$(document).ready(function () {
+  $("#slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: false,
   });
+
+  $("#slider").on("init", function (e, slick) {
+   $(".single-slide:first-child").addClass('animated');
+    doAnimations($firstAnimatingElements);
+  });
+  $("#slider").on("beforeChange", function (
+    e,
+    slick,
+    currentSlide,
+    nextSlide
+  ) {
+    $(".single-slide").removeClass('animated');
+    $('.single-slide[data-slick-index="' + nextSlide + '"]')
+    .addClass('animated');
+  });
+
+  
+});
